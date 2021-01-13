8857f771cd1953aabef3fcd81c1c0520f709d6f3
tejusasapu1247 <67572528+tejusasapu1247@users.noreply.github.com>
2021-01-13 15:53:17 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 
 	public static void main(String args[]){
-		for (int i=1;i<=10;i++){
+		for (int i=1;i<=3;i++){
 			System.out.println("Hello World....."+i);
 			System.out.println("Thanks for the webhooks demo..");
 			System.out.println("Thanks for the webhooks demo1..");
